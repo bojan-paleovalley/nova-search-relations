a4d95f311e2c8dff2b6339dfd0241d406892296d
bojan-paleovalley <bojanl@paleovalley.com>
2025-01-20 19:13:14 +0100

    Fixed method signature to match Laravel Nova v5

--- src/SearchesRelations.php
+++ src/SearchesRelations.php
@@ -77,7 +77,7 @@ trait SearchesRelations
      * @param  string  $search
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    protected static function applySearch($query, $search)
+    protected static function applySearch(\Illuminate\Contracts\Database\Eloquent\Builder $query, string $search): Builder
     {
         return $query->where(function ($query) use ($search) {
             parent::applySearch($query, $search);
